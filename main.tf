2cb3ed2355ce22b2a259d3a69ec6bede304ce420
rajuseven <158450667+rajuseven@users.noreply.github.com>
2025-09-10 17:33:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_names" {
+  default = [
+    "jenkins",
+    "tomcat-1",
+    "tomcat-2",
+    "Monitoring server"
+  ]
+}
+
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0bbdd8c17ed981ef9"
   instance_type          = "t2.micro"
   key_name               = "aws-devops-key"
   vpc_security_group_ids = ["sg-065591c7faf920d78"]
+
   tags = {
     Name = var.instance_names[count.index]
   }
-}
-
-variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
