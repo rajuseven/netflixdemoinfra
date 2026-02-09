6a0f62e318b74fb53ceb1c6c837051ae28d91e59
rajuseven <158450667+rajuseven@users.noreply.github.com>
2026-02-09 14:33:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0b6c6ebed2801a5cb"
   instance_type          = "t2.micro"
-  key_name               = "aws-devops-key"
-  vpc_security_group_ids = ["sg-065591c7faf920d78"]
+  key_name               = "terraform"
+  vpc_security_group_ids = ["sg-03f64b4d7b8850773"]
 
   tags = {
     Name = var.instance_names[count.index]
